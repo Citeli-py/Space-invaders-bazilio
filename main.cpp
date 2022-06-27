b60bbf83088948b3896181aa8e1b760e604aa754
rafaeltiribas <86385916+rafaeltiribas@users.noreply.github.com>
2022-06-27 19:34:37 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,76 @@
 #include <allegro.h>
-#include "classes.hpp"
 
+
+class projetil
+{
+	public:
+		int x, y, v;
+		int largura, altura;
+		BITMAP* tela;
+		
+	projetil(BITMAP* bmp, int xo, int yo, int dir)
+	{
+		tela = bmp;
+		largura = 5;
+		altura = 20;
+		x = xo;
+		y = yo;
+		v = 7*dir;
+	}
+	
+	void desenha()
+	{
+		int dir=1;
+		if(v<0) dir = -1;
+		//circle(tela, x, y-10, 5, makecol(255,0,0));
+		rectfill(tela, x, y, x+largura, y-dir*altura, makecol(255,0,0));
+	}
+	
+	void movimento()
+	{
+		y += v;
+	}
+};
+
+
+class jogador
+{
+	public:
+		int x,y;
+		int v;
+		BITMAP* tela;
+		int shot_cont;
+		
+	jogador(BITMAP* bmp, int xo, int yo)
+	{
+		shot_cont = 0;
+		tela = bmp;
+		x = xo;
+		y = yo;
+		v = 5;
+	}
+	
+	void movimento()
+	{
+		if(key[KEY_D]||key[KEY_RIGHT]) x+=v;
+		if(key[KEY_A]||key[KEY_LEFT]) x-=v;	
+	}
+	
+	projetil atirar()
+	{
+		shot_cont = 0;
+		projetil p(tela, x, y-15, -1);
+		return p;
+	}
+	
+	void desenha()
+	{
+		shot_cont++;
+		circlefill(tela, x, y, 10, makecol(255,255,255));
+		draw_sprite(screen, tela, 0, 0);
+		clear(tela);
+	}
+};
 
 void inicializa(BITMAP* bmp, projetil* tiros, int len)
 {
@@ -15,7 +85,6 @@ int main()
 	install_timer();
 	install_keyboard();
 	install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, NULL);
-	
 	set_color_depth(32);
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0);
 	set_window_title("Space Invaders");
@@ -27,16 +96,24 @@ int main()
 	projetil *tiros = (projetil*) malloc(len*sizeof(projetil));
 	inicializa(buffer, tiros, len);
 	
+	//Sons
+	SAMPLE *start = load_sample("inicio.wav");
+	SAMPLE *soundtrack = load_sample("soundtrack.wav");
+	SAMPLE *tiro = load_sample("tiro.wav");
+	play_sample(start, 100, 128, 1000, 0);
+	play_sample(soundtrack, 100, 128, 1000, 1);
+	
 	while (!key[KEY_ESC])
 	{
-		if(key[KEY_SPACE] && j.shot_cont >= 30)
+		if(key[KEY_SPACE] && j.shot_cont >= 20)
 		{
 			tiros[pos] = j.atirar();
 			pos++;
 			if(pos>=len) pos = 0;
+			play_sample(tiro, 100, 128, 1000, 0);
 		}
 		
-		for(int i=0; i<len; i++)
+		for(int i=0; i<len; i++) //Aqui tem um bug
 		{
 			if(tiros[i].x!=-99)
 			{
@@ -50,6 +127,9 @@ int main()
 		rest(10);
 	}
 	destroy_bitmap(buffer);
+	destroy_sample(start);
+	destroy_sample(soundtrack);
+	destroy_sample(tiro);
 	
 	return 0;
 }
