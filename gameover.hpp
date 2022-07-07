b8ee2299f41d200cef8215d682cc1d0dba59a8f4
Citeli-py <81636534+Citeli-py@users.noreply.github.com>
2022-07-07 15:00:13 -0300

    Update gameover.hpp

--- gameover.hpp
+++ gameover.hpp
@@ -1,16 +1,22 @@
 #include <allegro.h>
 
-void gameover(BITMAP* bmp, int pontos)
+int gameover(BITMAP* bmp, int pontos)
 {
     BITMAP* sprite = load_bitmap("sprite/gameover.bmp", NULL);
 
-    while (!key[KEY_ENTER])
+    while (!key[KEY_ESC])
     {
+    	if(key[KEY_ENTER]) return 1;
+    	
 		textprintf_ex(sprite, font, 320, 300, makecol(255,255,255), -1, "Voce fez %d pontos", pontos);
-		textprintf_ex(sprite, font, 310, 310, makecol(255,255,255), -1, "Aperte enter para sair");
+		textprintf_ex(sprite, font, 310, 310, makecol(255,255,255), -1, "Aperte ESC para sair");
+		textprintf_ex(sprite, font, 300, 320, makecol(255,255,255), -1, "Aperte ENTER para jogar de novo");
+		
 		draw_sprite(bmp, sprite, 0, 0);
         draw_sprite(screen, bmp, 0, 0);
 		clear(bmp);
     }
     rest(100);
+    
+    return 0;
 }
